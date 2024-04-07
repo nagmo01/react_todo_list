d3ba56fc2350ef8ebc18fba14b278ee19f359016
nagmo01 <bjworkshop01@gmail.com>
2024-04-08 08:57:44 +0900

    tailwindCSSで見た目を調整

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import './input.css';
 import { useState } from 'react';
 
 let count = 0;
@@ -12,11 +13,11 @@ function App() {
   const [editValue, setEditValue] = useState('');
 
   const addTodo = () => {
-    if (value === ''){
-      return ;
+    if (value === '') {
+      return;
     }
 
-    const newTodos = [...todos, {id: count, text: value, type: "task", completed: false }]
+    const newTodos = [...todos, { id: count, text: value, type: "task", completed: false }]
     setTodos(newTodos)
     setValue('')
     count += 1;
@@ -25,10 +26,10 @@ function App() {
 
   const onUpdate = (id) => {
     setTodos(todos.map((todo) => {
-      if ( todo.id === id ){
-        return {...todo, type: "task", text: editValue}
+      if (todo.id === id) {
+        return { ...todo, type: "task", text: editValue }
       } else {
-        return {...todo}
+        return { ...todo }
       }
     })
     )
@@ -65,11 +66,11 @@ function App() {
 
 
   const onCheck = (id) => {
-    setTodos(todos.map((todo)=> 
+    setTodos(todos.map((todo) =>
       todo.id === id ?
-        {...todo, completed: !todo.completed}
-        : 
-        {...todo}
+        { ...todo, completed: !todo.completed }
+        :
+        { ...todo }
     ))
 
   }
@@ -77,50 +78,57 @@ function App() {
 
   const list = todos.map((todo) => {
     return todo.type === "task" ? (
-      <li key={todo.id}>
-        <div className='border border-gray-400'>
-          <input className='border border-blue-200' type="checkbox" checked={todo.completed} onChange={()=>onCheck(todo.id)} />
-          <p>{todo.text}</p>
-          <button onClick={()=>onDelete(todo.id)}>削除</button>
-          <button onClick={()=>onEdit(todo.id)}>編集</button>
+      <div key={todo.id}>
+        <div className='my-3 flex justify-between'>
+          <div className='flex justify-start'>
+            <input className='self-center mx-5 form-checkbox h-6 w-6 text-green-500 border-gray-300 rounded' type="checkbox" checked={todo.completed} onChange={() => onCheck(todo.id)} />
+            <p className='me-5 self-center font-bold'>{todo.text}</p>
+          </div>
+          <div className='me-5'>
+            <button className='rounded bg-purple-400 px-5 py-2 my-3 text-white font-bold mr-2' onClick={() => onDelete(todo.id)}>削除</button>
+            <button className='rounded bg-green-400 px-5 py-2 my-3 text-white font-bold' onClick={() => onEdit(todo.id)}>編集</button>
+          </div>
         </div>
-      </li>
+      </div>
     ) : (
-        <li key={todo.id}>
-          <form>
-            <input type="text" value={editValue} onChange={onEditChange} />
-            <button onClick={()=>onUpdate(todo.id)}>確定</button>
-          </form>
-        </li>
-      );
+      <div className='flex justify-center mt-5' key={todo.id}>
+        <input className='font-bold w-2/3 border border-gray-600 border-4 rounded me-2 my-2' type="text" value={editValue} onChange={onEditChange} />
+        <button className="text-white rounded bg-red-400 font-bold px-3 py-3  my-2 px-9" onClick={() => onUpdate(todo.id)} >更新</button>
+      </div>
+    );
   });
+
 
 
   return (
     <>
-      <div className=''>
-        <div className=''>
-          <input className='' type="text" value={value} onChange={onChange} />
-          <button className='' onClick={addTodo} >送信する</button>
+      <h1 className='text-center font-bold text-3xl mt-20'>Todoアプリ</h1>
+      <div className='mt-12 text-2xl container mx-auto'>
+        <div className='flex justify-center mt-5'>
+          <input className='font-bold w-2/3 border border-black border-4 rounded me-2 my-2' type="text" value={value} onChange={onChange} />
+          <button className="text-white rounded bg-black font-bold px-3 py-3  my-2" onClick={addTodo} >追加する</button>
         </div>
 
-        <div>
-          {value}
-        </div>
-        <ul className='todoList flex'>
+        <div className='w-full mt-10'>
           {list}
-        </ul>
+        </div>
 
-        <div>
+        <div className='flex justify-around font-bold mt-12 mb-5'>
 
-          <p>タスクの数</p>
-          <p>{todos.length}</p>
+          <div className='flex'>
+            <p className=''>タスクの数：</p>
+            <p>{todos.length}</p>
+          </div>
 
-          <p>完了済み</p>
-          <p>{todos.filter((todo) => todo.completed === true ).length}</p>
+          <div className='flex'>
+            <p className=''>完了済み：</p>
+            <p>{todos.filter((todo) => todo.completed === true).length}</p>
+          </div>
 
-          <p>未完了</p>
-          <p>{todos.length - todos.filter((todo) => todo.completed === true ).length}</p>
+          <div className='flex'>
+            <p className=''>未完了：</p>
+            <p>{todos.length - todos.filter((todo) => todo.completed === true).length}</p>
+          </div>
 
         </div>
       </div>
